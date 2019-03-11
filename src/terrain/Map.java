d94e94685ca7e4694a43435a9ee0c1137ce46020
Joey <Joey@Joey-PC>
2019-03-10 23:53:03 -0700

    Update 1.0.14

--- src/terrain/Map.java
+++ src/terrain/Map.java
@@ -57,97 +57,35 @@ public class Map {
 					islandMask[i] = ((1 - ((smallestDistance) / 25000.0f)) / 2 + 0.2f);
 				}
 			}
-			for (int i = 0; i < 45; i++) {
-				int x = (i % 9);
-				int y = (i / 9);
-				if (id == MapID.PLAINS) {
-					mountain[x * 128][y * 128] = rand.nextFloat() / 3 + 0.6f;
-				} else if (id == MapID.RIVER) {
-					mountain[x * 128][y * 128] = Math.max(((1 - x) + (1 - y) + 10), x + y) * 0.07f;
-				} else if (id == MapID.ISLANDS) {
-					int pos = (y * 128) * 1025 + (x * 128);
-					mountain[x * 128][y * 128] = islandMask[pos];
-				} else if (id == MapID.SEA) {
-					if (x == 0 || x == 8) {
-						mountain[x * 128][y * 128] = .9f;
-					} else if (x == 1 || x == 7) {
-						mountain[x * 128][y * 128] = rand.nextFloat() * .5f + .25f;
-					} else {
-						if (x != 4) {
-							mountain[x * 128][y * 128] = (x - 4) * (x - 4) * 0.05f;
-						} else {
-							mountain[x * 128][y * 128] = 0;
-						}
-					}
-				} else if (id == MapID.MOUNTAIN) {
-					float r = rand.nextFloat();
-					if (r < 1) {
-						mountain[x * 128][y * 128] = (r * r * .8f) + .6f;
-					} else {
-						mountain[x * 128][y * 128] = r;
-					}
-				}
+
+			if (id == MapID.PLAINS) {
+				generatePlains();
+			} else if (id == MapID.RIVER) {
+				generateRiver();
+			} else if (id == MapID.ISLANDS) {
+				generateIslands();
+			} else if (id == MapID.SEA) {
+				generateSea();
+			} else if (id == MapID.MOUNTAIN) {
+				generateMountain();
 			}
-
-			for (int i = 2; i <= 8; i++) {
-				// int p: two to the power of i
-				// int r: how many squares there are
-				// int s: how many squares on the edge there are
-				int p = 1 << (8 - i);
-				int r = 1 << (2 * i);
-				int s = 2 << i;
-
-				// Interpolation for the squares on the top and left edges
-				for (int i2 = 0; i2 < 2 * r; i2++) {
-					int x = i2 % s * 2 * p;
-					int y = i2 / s * 2 * p;
-
-					float m = (rand.nextFloat() - .5f) / (3 << (i - 1));
-					if (x + 2 * p < Main.width + 1) {
-						mountain[x + p][y] = ((mountain[x][y] + mountain[x + 2 * p][y]) / 2) + m;
-					}
-					if (y + 2 * p < Main.height + 1) {
-						mountain[x][y + p] = ((mountain[x][y] + mountain[x][y + 2 * p]) / 2) + m;
-					}
+			int a = 128;
+			for (int i = 0; i < 1025 * 513; i++) {
+				int x = i % 1025;
+				int y = i / 1025;
+				int x1 = (int) (x / a) * a;
+				int y1 = (int) (y / a) * a;
+				if (x != x1 || y != y1) {
+					mountain[x][y] = bicosineInterpolation(x1, y1, a, x, y);
 				}
-				// Interpolation for the rest
-				for (int i2 = 0; i2 < 2 * r; i2++) {
-					int x = i2 % s * 2 * p;
-					int y = i2 / s * 2 * p;
-					// m: a random
-
-					float m = 0;
-					if (i < 4) {
-						m = (rand.nextFloat() - .5f) / (4 << i);
-						if (x + 2 * p < Main.width + 1 && y + 2 * p < Main.height + 1) {
-							mountain[x + p][y + 2 * p] = ((mountain[x][y + 2 * p] + mountain[x + 2 * p][y + 2 * p]) * .5f + m);
-
-							mountain[x + 2 * p][y + p] = ((mountain[x + 2 * p][y] + mountain[x + 2 * p][y + 2 * p]) * .5f + m);
-
-							mountain[x + p][y + p] = ((mountain[x + p][y + 2 * p] + mountain[x + 2 * p][y + p] + mountain[x][y + p] + mountain[x + p][y]) * .25f + m);
-						}
-					} else {
-						m = (rand.nextFloat() - .5f) / (2 << i);
-						if (x + 2 * p < Main.width + 1 && y + 2 * p < Main.height + 1) {
-							mountain[x + p][y + 2 * p] = ((mountain[x][y + 2 * p] + mountain[x + 2 * p][y + 2 * p]) * .5f) + m;
-
-							mountain[x + 2 * p][y + p] = ((mountain[x + 2 * p][y] + mountain[x + 2 * p][y + 2 * p]) * .5f) + m;
-
-							mountain[x + p][y + p] = ((mountain[x + p][y + 2 * p] + mountain[x + 2 * p][y + p] + mountain[x][y + p] + mountain[x + p][y]) * .25f) + m;
-						}
-					}
-				}
-
 			}
 		}
-		for(int i = 0; i < 1025*513; i++){
+		for (int i = 0; i < 1025 * 513; i++) {
 			int x = i % 1025;
 			int y = i / 1025;
-			mountain[x][y] = transform(mountain[x][y],1.5f);
-			if(mountain[x][y] > 1.5)
-				mountain[x][y] = 1.5f;
-			if(mountain[x][y] < 0)
-				mountain[x][y] = 0f;
+			mountain[x][y] = transform(mountain[x][y], 1.5f);
+			if (mountain[x][y] > 1.5) mountain[x][y] = 1.5f;
+			if (mountain[x][y] < 0) mountain[x][y] = 0f;
 		}
 		// colors the MapArray
 		for (int i = 0; i < 1025 * 513; i++) {
@@ -161,10 +99,101 @@ public class Map {
 		}
 	}
 
+	void generateMountain() {
+		for (int i = 0; i < 45; i++) {
+			int x = (i % 9);
+			int y = (i / 9);
+			float r = rand.nextFloat();
+			if (r < 1) {
+				mountain[x * 128][y * 128] = (r * r * .8f) + .6f;
+			} else {
+				mountain[x * 128][y * 128] = r;
+			}
+		}
+	}
+
+	void generatePlains() {
+		for (int i = 0; i < 45; i++) {
+			int x = (i % 9);
+			int y = (i / 9);
+			mountain[x * 128][y * 128] = rand.nextFloat() / 3 + 0.6f;
+		}
+	}
+
+	void generateIslands() {
+		for (int i = 0; i < 45; i++) {
+			int x = (i % 9);
+			int y = (i / 9);
+			int pos = (y * 128) * 1025 + (x * 128);
+			mountain[x * 128][y * 128] = islandMask[pos];
+		}
+	}
+
+	void generateRiver() {
+		for (int i = 0; i < 45; i++) {
+			int x = (i % 9);
+			int y = (i / 9);
+			mountain[x * 128][y * 128] = Math.max(((1 - x) + (1 - y) + 10), x + y) * 0.07f;
+		}
+	}
+	
+	void generateSea(){
+		for (int i = 0; i < 45; i++) {
+			int x = (i % 9);
+			int y = (i / 9);if (x == 0 || x == 8) {
+				mountain[x * 128][y * 128] = .9f;
+			} else if (x == 1 || x == 7) {
+				mountain[x * 128][y * 128] = rand.nextFloat() * .5f + .25f;
+			} else {
+				if (x != 4) {
+					mountain[x * 128][y * 128] = (x - 4) * (x - 4) * (x - 4) * (x - 4) * 0.003124f;
+				} else {
+					mountain[x * 128][y * 128] = 0;
+				}
+			}
+		}
+	}
+
 	float transform(float r, float nonlinearity) {
-		double verticalStretch = 1.01311/nonlinearity-.0787654;
-		double newHeight = (verticalStretch*Math.tan(nonlinearity*r-(nonlinearity*0.5))+0.5);
-		return (float)newHeight;
+		double verticalStretch = 0.762799 / nonlinearity - .0787654;
+		double newHeight = (verticalStretch * Math.tan(nonlinearity * r - (nonlinearity * 0.5)) + 0.5);
+		if (newHeight > 1) newHeight = 2 * (r - 1) * (r - 1) + 1;
+		return (float) newHeight;
+	}
+
+	float bicosineInterpolation(int x1, int y1, int p, int x2, int y2) {
+		// (x1,y1)- coords of top left corner of box
+		// p- length/heigth pf box
+		// (x3,y3)- coords of point
+		if (x1 + p > 1025 || y1 + p > 513) {
+			return 1.5f;
+		}
+		float topInterpolation = cosineInterpolation(x1, mountain[(int) x1][(int) y1], x1 + p, mountain[(int) (x1 + p)][(int) y1], x2);
+		float bottomInterpolation = cosineInterpolation(x1, mountain[(int) x1][(int) (y1 + p)], x1 + p, mountain[(int) (x1 + p)][(int) (y1 + p)], x2);
+		return cosineInterpolation(y1, topInterpolation, y1 + p, bottomInterpolation, y2);
+	}
+
+	float cosineInterpolation(int x1, float y1, int x2, float y2, int m) {
+		double xDiff = (x2 - x1);
+		double mu2 = (1 - Math.cos((m / xDiff - x1 / xDiff) * Math.PI)) / 2;
+		double y3 = (y1 * (1 - mu2) + y2 * mu2);
+		return (float) y3;
+	}
+
+	float bilinearInterpolation(int x1, int y1, int p, int x2, int y2) {
+		// (x1,y1)- coords of top left corner of box
+		// p- length/heigth pf box
+		// (x3,y3)- coords of point
+		if (x1 + p > 1025 || y1 + p > 513) {
+			return 1.5f;
+		}
+		float topInterpolation = linearInterpolation(x1, mountain[(int) x1][(int) y1], x1 + p, mountain[(int) (x1 + p)][(int) y1], x2);
+		float bottomInterpolation = linearInterpolation(x1, mountain[(int) x1][(int) (y1 + p)], x1 + p, mountain[(int) (x1 + p)][(int) (y1 + p)], x2);
+		return linearInterpolation(y1, topInterpolation, y1 + p, bottomInterpolation, y2);
+	}
+
+	float linearInterpolation(int x1, float y1, int x2, float y2, int m) {
+		return (y2 - y1) / (x2 - x1) * (m - x1) + y1;
 	}
 
 	// getArray(...): returns the float value at a given coordinate
@@ -217,10 +246,10 @@ public class Map {
 			green = (int) (value * value * value * 85);
 			red = (int) (value * value * value * 85);
 		}
-//
-//		blue = (int) (value * 255);
-//		green = (int) (value * 255);
-//		red = (int) (value * 255);
+		//
+		// blue = (int) (value * 255);
+		// green = (int) (value * 255);
+		// red = (int) (value * 255);
 
 		if (blue < 0) blue = 0;
 		if (green < 0) green = 0;
